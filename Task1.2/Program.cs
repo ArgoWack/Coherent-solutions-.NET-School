e62ea646048c1e0a85f44d85a4aef7d54ff2a9c1
ArgoWack <45313830+ArgoWack@users.noreply.github.com>
2024-10-20 11:35:50 +0200

    I'm sorry for my previous oversight on my part too. Now it should work well since reverseRemainder can take values from range 0-11, and all of them are adressed in switch statements.

--- Task1.2/Program.cs
+++ Task1.2/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System.Linq.Expressions;
+using static System.Console;
 /*
 Task  1.2
 Let 10-character ISBN is an unique digital code to identify a book, with form: . Digit  is a control one, it is calculated according the condition, that expression 
@@ -39,7 +40,17 @@ class Program
         //  11 minus remainder of the sum after divide by 11 to get reverse reminder
         int reverseRemainder = 11 - sum % 11;
 
-        // if remainder is 10, returns 'X'. Otherwise, returns the reverse of remainder as a digit
-        return reverseRemainder == 10 ? 'X' : reverseRemainder.ToString()[0];
+        switch (reverseRemainder)
+        {
+            case 11:
+                //if remainder is 11, returns '0'.
+                return '0';
+            case 10:
+                //if remainder is 10, returns 'X'.
+                return 'X';
+            default:
+                //when remainder is between 0-9, return 0-9 accordingly
+                return reverseRemainder.ToString()[0];
+        }
     }
 }
