296171f8eda894229e39b1a085b9302dbbf75ecd
ArgoWack <45313830+ArgoWack@users.noreply.github.com>
2024-10-25 18:29:06 +0200

    Separated classes into a different files.

---
+++ Task2.3/PracticalLesson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class PracticalLesson : TrainingComponent
+{
+    public string TaskLink { get; set; }
+    public string SolutionLink { get; set; }
+
+    public PracticalLesson(string description, string taskLink, string solutionLink) : base(description)
+    {
+        TaskLink = taskLink;
+        SolutionLink = solutionLink;
+    }
+
+    // Implementing deep cloning
+    public override TrainingComponent Clone()
+    {
+        return new PracticalLesson(Description, TaskLink, SolutionLink);
+    }
+}
