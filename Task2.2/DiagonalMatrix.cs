64da73f8d1fbb94f1dd5759a47fb82fcea4f13eb
ArgoWack <45313830+ArgoWack@users.noreply.github.com>
2024-10-25 18:23:10 +0200

    Changed Add method into extension and name of validation method from size to index according to review.

--- Task2.2/DiagonalMatrix.cs
+++ Task2.2/DiagonalMatrix.cs
@@ -1,30 +1,30 @@
 ﻿using System;
 
-    public class DiagonalMatrix
+public class DiagonalMatrix
+{
+    private readonly int[] diagonalElements;
+
+    // readonly property to get the size of the matrix
+    public int Size { get; }
+
+    // constructor
+    public DiagonalMatrix(params int[] elements)
     {
-        private readonly int[] diagonalElements;
-
-        // readonly property to get the size of the matrix
-        public int Size { get; }
-
-        // constructor
-        public DiagonalMatrix(params int[] elements)
+        if (elements == null)
         {
-            if (elements == null)
-            {
-                Size = 0;
-                diagonalElements = new int[0];
-            }
-            else
-            {
-                Size = elements.Length;
-                diagonalElements = new int[Size];
-                // simplified to using Array.Copy instead of loop according to review
-                Array.Copy(elements, diagonalElements, Size);
-            }
+            Size = 0;
+            diagonalElements = new int[0];
         }
+        else
+        {
+            Size = elements.Length;
+            diagonalElements = new int[Size];
+            // simplified to using Array.Copy instead of loop according to review
+            Array.Copy(elements, diagonalElements, Size);
+        }
+    }
     //Added separate method for validation
-    private bool IsProperSize(int i, int j)
+    private bool IsProperIndex(int i, int j)
     {
         if (i < 0 || i >= Size || j < 0 || j >= Size || j != i)
         {
@@ -34,75 +34,58 @@
     }
     // indexer
     public int this[int i, int j]
+    {
+        get
         {
-            get
+            if (!IsProperIndex(i, j))
             {
-                if (!IsProperSize(i, j))
-                {
-                    return 0;
-                }
-                return diagonalElements[i];
+                return 0;
             }
-            set
-            {
-                if (!IsProperSize(i, j))
-                {
-                    throw new ArgumentOutOfRangeException("Indices are out of bounds.");
-                }
-                diagonalElements[i] = value;
-            }
+            return diagonalElements[i];
         }
-
-        // method to sum elements on diagonal (Track)
-        public int Track()
+        set
         {
-            int sum = 0;
+            if (!IsProperIndex(i, j))
+            {
+                throw new ArgumentOutOfRangeException("Indices are out of bounds.");
+            }
+            diagonalElements[i] = value;
+        }
+    }
+
+    // method to sum elements on diagonal (Track)
+    public int Track()
+    {
+        int sum = 0;
+        for (int i = 0; i < Size; i++)
+        {
+            sum += diagonalElements[i];
+        }
+        return sum;
+    }
+    public override bool Equals(object obj)
+    {
+        if (obj is DiagonalMatrix otherMatrix)
+        {
+            if (Size != otherMatrix.Size)
+            {
+                return false;
+            }
+
             for (int i = 0; i < Size; i++)
             {
-                sum += diagonalElements[i];
-            }
-            return sum;
-        }
-        public override bool Equals(object obj)
-        {
-            if (obj is DiagonalMatrix otherMatrix)
-            {
-                if (Size != otherMatrix.Size)
+                if (diagonalElements[i] != otherMatrix.diagonalElements[i])
                 {
                     return false;
                 }
-
-                for (int i = 0; i < Size; i++)
-                {
-                    if (diagonalElements[i] != otherMatrix.diagonalElements[i])
-                    {
-                        return false;
-                    }
-                }
-                return true;
             }
-            return false;
+            return true;
         }
-
-        // Adds matrices treating missing elements as 0's
-        public DiagonalMatrix Add(DiagonalMatrix other)
-        {
-            if (other == null)
-            {
-                //in case of other martix being null it returns copy of current matrix resulting in no changes
-                return new DiagonalMatrix((int[])this.diagonalElements.Clone());
-            }
-
-            int maxSize = Math.Max(this.Size, other.Size);
-            int[] resultElements = new int[maxSize];
-
-            for (int i = 0; i < maxSize; i++)
-            {
-                int value1 = i < this.Size ? this[i, i] : 0;
-                int value2 = i < other.Size ? other[i, i] : 0;
-                resultElements[i] = value1 + value2;
-            }
-
-            return new DiagonalMatrix(resultElements);
-        }
+        return false;
     }
+
+    public int[] GetDiagonalElements()
+    {
+        return (int[])diagonalElements.Clone();
+    }
+}
