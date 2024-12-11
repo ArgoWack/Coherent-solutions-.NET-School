75c6aa43bedc90a2782af10fd683e553de336e78
ArgoWack <45313830+ArgoWack@users.noreply.github.com>
2024-12-11 16:58:47 +0100

    Added const Max_Length

--- Task6/Author.cs
+++ Task6/Author.cs
@@ -2,17 +2,18 @@
 {
     public class Author
     {
+        const int MAX_LENGTH = 200;
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Author() { }
         public Author(string firstName, string lastName, DateTime dateOfBirth)
         {
-            if (string.IsNullOrWhiteSpace(firstName) || firstName.Length > 200)
-                throw new ArgumentException("First name cannot be empty, null, or more than 200 characters.");
+            if (string.IsNullOrWhiteSpace(firstName) || firstName.Length > MAX_LENGTH)
+                throw new ArgumentException($"First name cannot be empty, null, or more than {MAX_LENGTH} characters.");
 
-            if (string.IsNullOrWhiteSpace(lastName) || lastName.Length > 200)
-                throw new ArgumentException("Last name cannot be empty, null, or more than 200 characters.");
+            if (string.IsNullOrWhiteSpace(lastName) || lastName.Length > MAX_LENGTH)
+                throw new ArgumentException($"Last name cannot be empty, null, or more than {MAX_LENGTH} characters.");
 
             FirstName = firstName;
             LastName = lastName;
